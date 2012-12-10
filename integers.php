343cb1943e06e54e4f083fdcf8117678b77eebe7
Stephen Woods <saw@stephenwoods.net>
2012-12-09 23:38:11 -0800

    asseting things in comments

--- integers.php
+++ integers.php
@@ -1,8 +1,10 @@
 <?php
 
-function massProduce(){	
-	
-	$max = 3000000000000000;
+function massProduce(){ 
+    
+    //It would take 500 years at 10000 integers per second to have a 50%
+    //chance of a collission
+    $max = 3000000000000000;
     do{
         $result = floor($max*(hexdec(bin2hex(openssl_random_pseudo_bytes(4)))/0xffffffff));
     }while($result == $max);
